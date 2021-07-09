28af2b654641bf0dee291377f640d1cb85fb4faa
dessant <git@armin.dev>
2021-07-09 21:55:33 +0300

    chore: update package

--- dist/index.js
+++ dist/index.js
@@ -24384,7 +24384,7 @@ class App {
         issue,
         {active: issueData.locked, reason: issueData.active_lock_reason},
         () =>
-          this.client.issues
+          this.client.rest.issues
             .createComment({...issue, body: commentBody})
             .catch(err => core.warning(err.toString()))
       );
@@ -24392,7 +24392,7 @@ class App {
 
     if (this.config['close-issue'] && issueData.state === 'open') {
       core.debug(`Closing (issue: ${issue.issue_number})`);
-      await this.client.issues.update({...issue, state: 'closed'});
+      await this.client.rest.issues.update({...issue, state: 'closed'});
     }
 
     if (this.config['lock-issue'] && !issueData.locked) {
@@ -24410,7 +24410,7 @@ class App {
       } else {
         params = issue;
       }
-      await this.client.issues.lock(params);
+      await this.client.rest.issues.lock(params);
     }
   }
 
@@ -24424,12 +24424,12 @@ class App {
 
     if (this.config['close-issue'] && issueData.state === 'closed') {
       core.debug(`Reopening (issue: ${issue.issue_number})`);
-      await this.client.issues.update({...issue, state: 'open'});
+      await this.client.rest.issues.update({...issue, state: 'open'});
     }
 
     if (this.config['lock-issue'] && issueData.locked) {
       core.debug(`Unlocking (issue: ${issue.issue_number})`);
-      await this.client.issues.unlock(issue);
+      await this.client.rest.issues.unlock(issue);
     }
   }
 
@@ -24444,18 +24444,18 @@ class App {
     const issue = {...github.context.repo, issue_number: issueData.number};
 
     core.debug(`Unlabeling (issue: ${issue.issue_number})`);
-    await this.client.issues.removeLabel({...issue, name: supportLabel});
+    await this.client.rest.issues.removeLabel({...issue, name: supportLabel});
 
     if (this.config['lock-issue'] && issueData.locked) {
       core.debug(`Unlocking (issue: ${issue.issue_number})`);
-      await this.client.issues.unlock(issue);
+      await this.client.rest.issues.unlock(issue);
     }
   }
 
   async ensureUnlock(issue, lock, action) {
     if (lock.active) {
       if (!lock.hasOwnProperty('reason')) {
-        const {data: issueData} = await this.client.issues.get({
+        const {data: issueData} = await this.client.rest.issues.get({
           ...issue,
           headers: {
             Accept: 'application/vnd.github.sailor-v-preview+json'
@@ -24463,7 +24463,7 @@ class App {
         });
         lock.reason = issueData.active_lock_reason;
       }
-      await this.client.issues.unlock(issue);
+      await this.client.rest.issues.unlock(issue);
 
       let actionError;
       try {
@@ -24481,7 +24481,7 @@ class App {
           }
         };
       }
-      await this.client.issues.lock(issue);
+      await this.client.rest.issues.lock(issue);
 
       if (actionError) {
         throw actionError;
